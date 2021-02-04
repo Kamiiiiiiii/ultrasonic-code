3d81e3f7ce8f608c9554c8d1f119c3fdf5f2178b
Kamiiiiiiii <78355572+Kamiiiiiiii@users.noreply.github.com>
2021-02-04 18:51:30 +0500

    ultrasonic coding

---
+++ ultrasonic_code.ino
@@ -0,0 +1,22 @@
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600);
+  Serial.println("Ultrasonic Sensor HC-SR04 Test");
+  Serial.println("with Arduino UNO R3");
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+}
